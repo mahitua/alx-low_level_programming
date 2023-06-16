6f206bc4d44d0e70437acb39ddeb850c2b14dd89
mahitua <melozenebe77@gmail.com>
2023-06-16 14:44:40 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
- /**
- * main- entry point
- * Description: '-print lowercase letters except e and q'
- * Return:  Always 0
- */
+/**
+* main- entry point
+* Description: 'print lowercase letters except e and q'
+* return:  Always 0
+**/
 int main(void)
 {
 char letter = 'a';
+
 while (letter <= 'z')
-{ 
+{
 	if (letter != 'q' && letter != 'e')
 	{
 		putchar(letter);
 	}
 	letter++;
-} 
+}
 putchar('\n');
 return (0);
 }
