69de9c6e3b3f5a8945d7fe6e6196bc2a6c5be3f3
mahitua <melozenebe77@gmail.com>
2023-06-16 13:50:12 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,14 @@
 * Description: '-print lowercase letters'
 * Return:  Always 0
 */
-int main (void)
+int main(void)
 {
 	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-	putchar(ch);
+		putchar(ch);
 	}
-	putchar('\n');
+		putchar('\n');
 	return (0);
 }
