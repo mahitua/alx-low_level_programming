e5ed02488154fad4c6fc431a0af97fda7d8934ec
mahitua <melozenebe77@gmail.com>
2023-06-16 13:35:17 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
-
-
-
-
-
-int main () {
-	   char ch;
-
-	      for(ch = 'a' ; ch <= 'z' ; ch++) {
-		            putchar(ch);
-			       }
+int main (void)
+{
+	char ch;
+	for(ch = 'a' ; ch <= 'z' ; ch++)
+		{
+		putchar(ch);
+		}
 	         putchar('\n');
 	         return (0);
 }
