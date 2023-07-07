7a1f76076335b4f47ad62188ff4c9007a31786f0
mahitua <melozenebe77@gmail.com>
2023-07-07 06:56:55 -0700

    main.h

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+void _puts(char *str)
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return : Always 0(Success)
+ */
+int main(void)
+{
+	puts("\"programming is nice language that requires practice");
+	return (0);
+}
+
