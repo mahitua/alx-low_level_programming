d3a2ec55aec16a31209f6f2de2e284c8ae74fbef
mahitua <melozenebe77@gmail.com>
2023-07-21 13:17:45 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,10 +10,10 @@ void print_all(const char * const format, ...)
 {
 	int i = 0;
 	char *str, *sep = "";
-
 	va_list list;
+
 	va_start(list, format);
-	if (format)
+		if (format)
 	{
 		while (format[i])
 		{
