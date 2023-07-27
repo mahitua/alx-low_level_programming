6601729164f1087964eb9e19094906835e628551
mahitua <melozenebe77@gmail.com>
2023-07-27 13:44:23 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -25,10 +25,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->len = len;
 	new->next = NULL;
 	if (*head == NULL)
-	{
-		*head = new;	
-		return (new);
-	}
+		*head = new;
+	return (new);
+}
 	while (temp->next)
 		temp = temp->next;
 	temp->next = new;
